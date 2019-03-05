ae5a4fc95f68426ac2578e50b9e63def8b153911
liguangyi <252438823@qq.com>
2019-03-05 23:37:31 +0800

    update

--- src/game_dashboard/components/CardPanel/index.js
+++ src/game_dashboard/components/CardPanel/index.js
@@ -11,12 +11,12 @@ export default function CardPanel() {
   return (
     <div className={style.panel}>
       <div className={style.controller}>
-        <ButtonGroup style={{ float: 'left' }}>
+        <ButtonGroup className={style.dateFilter}>
           <Button>This Month</Button>
           <Button>Next Month</Button>
           <Button>This Year</Button>
         </ButtonGroup>
-        <ButtonGroup style={{ float: 'right' }}>
+        <ButtonGroup className={style.layoutSelector}>
           <Tooltip content="Grid" position={Position.BOTTOM}>
             <Button icon="layout-grid" active />
           </Tooltip>
