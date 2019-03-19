a0c252cac416ef6adcbd0dc6a79b68ce952700bd
liguangyi <252438823@qq.com>
2019-03-19 23:25:58 +0800

    update

--- src/admin_dashboard/components/GameList/index.js
+++ src/admin_dashboard/components/GameList/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Alert } from '@blueprintjs/core'
+import { Cell, Column, Table } from '@blueprintjs/table'
 
 // import GameForm from '../GameForm'
 import PageLoading from '../../../common/components/PageLoading'
@@ -7,29 +9,77 @@ export default class GameList extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      loading: true
+      loading: true,
+      data: null,
+      error: null
     }
     this.db = firebase.firestore()
+  }
+
+  componentDidMount() {
     this.query()
   }
 
   query = () => {
+    this.setState({
+      loading: true
+    })
     this.db
       .collection('games')
       .get()
       .then(querySnapshot => {
+        const data = {}
         querySnapshot.forEach(doc => {
-          // doc.data() is never undefined for query doc snapshots
-          console.log(doc.id, ' => ', doc.data())
+          data[doc.id] = doc.data()
+        })
+        this.setState({
+          loading: false,
+          data
+        })
+      })
+      .catch(error => {
+        this.setState({
+          loading: false,
+          error
         })
       })
   }
 
+  retry = () => {
+    this.setState(
+      {
+        error: null
+      },
+      this.query
+    )
+  }
+
   render() {
-    const { loading } = this.state
+    const { loading, data, error } = this.state
     if (loading) {
       return <PageLoading />
     }
-    return <div />
+
+    if (error) {
+      return (
+        <Alert
+          icon="error"
+          canEscapeKeyCancel={false}
+          canOutsideClickCancel={false}
+          confirmButtonText="Retry"
+          onConfirm={this.retry}
+          isOpen
+        >
+          <p>Get Data Failed: {error.toString()}</p>
+        </Alert>
+      )
+    }
+    return (
+      <div>
+        <Table numRows={data.length}>
+          {/* <Column name="Dollars" cellRenderer={cellRenderer} /> */}
+        </Table>
+      </div>
+    )
   }
 }
